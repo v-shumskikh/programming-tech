1c44b2a282661319ae7ee7e1990fafa7d433dfae
v-shumskikh <v.shumskikh@sibstrin.ru>
2026-05-16 00:47:24 +0700

    Добавлен класс BankAccount и его использование в Program

---
+++ ProgrammingTech/Lab1/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab1;
+
+public class BankAccount
+{
+    // Счётчик для автогенерации уникальных номеров счетов
+    private static int s_accountNumberSeed = 1234567890;
+
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+
+        Owner = name;
+        Balance = initialBalance;
+    }
+}
--- ProgrammingTech/Lab1/Program.cs
+++ ProgrammingTech/Lab1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab1;
+
+// Создаём счёт с начальным балансом
+var account = new BankAccount("Иван Петров", 1000);
+
+Console.WriteLine($"Счёт {account.Number}");
+Console.WriteLine($"Владелец: {account.Owner}");
+Console.WriteLine($"Баланс: {account.Balance}");
